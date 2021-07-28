1f56c297517222cca3a727083e40123ab7ec1624
97ianlzy <86585262+97ianlzy@users.noreply.github.com>
2021-07-28 22:56:06 +0800

    Update index.js

--- src/components/new-game-screen/index.js
+++ src/components/new-game-screen/index.js
@@ -5,19 +5,13 @@ class NewGameScreen extends ScreenComponent {
 	render () {
 		return `
 			<button class="top left" data-action="back"><span class="fas fa-arrow-left"></span></button>
-			<h2 data-string="Load">Load</h2>
-			<div data-ui="saveSlots">
-				<h3 data-string="LoadSlots">Saved Games</h3>
+			<h2 data-string="New">New Game</h2>
+			<div data-ui="chapterSelect">
+				<h3 data-string="ChapterSelect">Select Chapter</h3>
 				<div data-ui="slots" class="row row--spaced padded">
-					<slot-container label="${this.engine.setting ('SaveLabel')}" type="load"></slot-container>
+					<slot-container label="${this.engine.setting ('ChapterLabel')}" type="load"></slot-container>
 				</div>
 			</div>
-			${ autoSaveEnabled ? `<div data-ui="autoSaveSlots">
-				<h3 data-string="LoadAutoSaveSlots">Auto Saved Games</h3>
-				<div data-ui="slots" data-content="slots" class="row row--spaced padded">
-					<slot-container label="${this.engine.setting ('AutoSaveLabel')}" type="load"></slot-container>
-				</div>
-			</div>` : ''}
 		`;
 	}
 }
