7b9f926bc2bd8b8a54025dd26e614e031ea58d0b
Drkbai <87938773+Drkbai@users.noreply.github.com>
2021-08-23 23:23:46 +0800

    Add files via upload
    Add new language

---
+++ src/translations/繁體中文.js
@@ -0,0 +1,91 @@
+/**
+ * ============================================================
+ * 繁體中文 (Traditional Chinese)
+ * ============================================================
+ *
+ * Translators:
+ *
+ * Drkbai
+ */
+
+export default {
+	'AdvanceHelp': '按下空格鍵或點擊屏幕以繼續',
+	'AllowPlayback': '點擊這裡以啟用語音回放',
+	'Audio': '音效',
+	'AutoPlay': '自動',
+	'AutoPlayButton': '進入自動播放',
+	'AutoPlaySpeed': '自動播放速度',
+
+	'Back': '後退',
+	'BackButton': '後退',
+
+	'Cancel': '取消',
+	'Close': '關閉',
+	'Confirm': '確定要退出嗎？',
+	'Credits': 'Credits',
+
+	'Delete': '刪除',
+	'DialogLogButton': 'Show the dialog log',
+
+	'FullScreen': '全屏顯示',
+
+	'Gallery': '畫廊',
+
+	'Help': '幫助',
+	'Hide': '隱藏',
+	'HideButton': '隱藏文字',
+
+	'iOSAudioWarning': 'iOS暫不支持音效設定',
+
+	'KeyboardShortcuts': '鍵盤快捷鍵',
+
+	'Language': '語言',
+	'Load': '讀取',
+	'LoadAutoSaveSlots': '自動儲存的遊戲進度',
+	'LoadButton': '顯示讀取界面',
+	'Loading': '加載中',
+	'LoadingMessage': '等待素材加載中',
+	'LoadSlots': '儲存的遊戲進度',
+	'LocalStorageWarning': '該瀏覽器暫不支持本地儲存功能',
+	'Log': 'Log',
+
+	'Music': '音樂音量',
+
+	'NewContent': '沒有儲存的遊戲進度',
+	'NoSavedGames': '沒有自動儲存的遊戲進度',
+	'NoAutoSavedGames': '有新的內容可供使用，重新加載頁面以獲取最新版本',
+	'NoDialogsAvailable': 'No dialogs available. Dialogs will appear here as they show up',
+
+	'OK': '確定',
+	'OrientationWarning': '請將設備旋轉以體驗遊戲內容',
+	'Overwrite': '覆蓋',
+
+	'QuickButtons': '快捷菜單按鈕',
+	'QuickMenu': '快捷菜單',
+	'Quit': '退出',
+	'QuitButton': '退出遊戲',
+
+	'Resolution': '分辨率',
+
+	'Save': '存檔',
+	'SaveButton': '顯示存檔界面',
+	'SaveInSlot': '寫入存檔槽位t',
+	'SelectYourLanguage': '選擇語言',
+	'Settings': '環境設定',
+	'SettingsButton': '顯示環境設定界面',
+	'Show': '顯示',
+	'Skip': '跳過',
+	'SkipButton': '進入跳過模式',
+	'SlotDeletion': '確定要刪除這個存檔槽位嗎？',
+	'SlotOverwrite': '确定要覆盖这个存檔槽位嗎？',
+	'Sound': '音效音量',
+	'Start': '開始',
+	'Stop': '停止',
+
+	'TextSpeed': '文字顯示速度',
+
+	'Video': '視頻音量',
+	'Voice': '語音音量',
+
+	'Windowed': '窗口'
+};
